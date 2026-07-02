9d7153cfad5a462a4574938d828e6061d7074a30
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-07-03 00:43:25 +0530

    Create 5.py

---
+++ Python Data Structures/Dictionary/5.py
@@ -0,0 +1,6 @@
+# wap to prepare a dictionary where the keys are numbers between 1 and 15 (both included) and the values are square of the keys
+dict = {}
+for i in range(1,16):
+    dict[i] = i ** 2
+
+print(dict)
