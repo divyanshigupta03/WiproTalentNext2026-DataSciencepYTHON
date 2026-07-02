f977cb475afe31161da3fe34bc91e216b28ce131
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-07-03 00:43:46 +0530

    Create 6.py

---
+++ Python Data Structures/Dictionary/6.py
@@ -0,0 +1,7 @@
+# wap to sum all the values in a dictionary, considering the values will be of int type
+
+d = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+
+total = sum(d.values())
+
+print("Sum of all values:", total)
