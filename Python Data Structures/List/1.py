83d9577a48fcc000d58b6c9d16d3d89595e1fcf4
divyanshigupta03 <divyanshigupta1103@gmail.com>
2026-07-03 00:37:52 +0530

    Create 1.py

---
+++ Python Data Structures/List/1.py
@@ -0,0 +1,10 @@
+# Write a program to create a list of 5 integers and display the list items. Access individual elements through index
+numbers = [10,20,30,40,50]
+
+print("List",numbers)
+
+print("First element" , numbers[0])
+print("Second element" , numbers[1])
+print("Third element" , numbers[2])
+print("Fourth element" , numbers[3])
+print("Fifth element" , numbers[4])
